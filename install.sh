4f4fa530770fb4eb57a8dd913e5179e2a01e4652
yulinzhihou <yulinzhihou@gmail.com>
2021-02-01 16:17:11 +0800

    init

---
+++ install.sh
@@ -0,0 +1,193 @@
+#!/usr/bin/env bash
+# Author: yulinzhihou <yulinzhihou@gmail.com>
+# Forum:  https://gsgamesahre.com
+# Date :  2021-02-01
+# Notes:  GS_TL_Env for CentOS/RedHat 7+ Debian 8+ and Ubuntu 14+
+
+# 第一步：更新系统组件，安装docker,docker-composer
+# 第二步：下载GS相关命令到系统
+# 第三步：在线下载打包好的镜像或者导入离线版本的镜像
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+printf "
+#######################################################################
+#       GS_TL_Env 支持： CentOS/RedHat 7+  Ubuntu 16+ Debian 8+       #
+#       更多天龙网游单机版本请访问：          https://gsgamesahre.com    #
+#       技术交流群：                        826717146                  #
+#######################################################################
+"
+# 颜色代码
+color_code()
+{
+  echo=echo
+  for cmd in echo /bin/echo; do
+    $cmd >/dev/null 2>&1 || continue
+    if ! $cmd -e "" | grep -qE '^-e'; then
+      echo=$cmd
+      break
+    fi
+  done
+  CSI=$($echo -e "\033[")
+  CEND="${CSI}0m"
+  CDGREEN="${CSI}32m"
+  CRED="${CSI}1;31m"
+  CGREEN="${CSI}1;32m"
+  CYELLOW="${CSI}1;33m"
+  CBLUE="${CSI}1;34m"
+  CMAGENTA="${CSI}1;35m"
+  CCYAN="${CSI}1;36m"
+  CSUCCESS="$CDGREEN"
+  CFAILURE="$CRED"
+  CQUESTION="$CMAGENTA"
+  CWARNING="$CYELLOW"
+  CMSG="$CCYAN"
+}
+color_code
+# 检测是不是root用户。不是则退出
+[ $(id -u) != "0" ] && { echo "${CFAILURE}错误: 你必须使用${CEND}"; exit 1; }
+
+# 检测系统版本
+check_os()
+{
+  if [ -e "/usr/bin/yum" ]; then
+    PM=yum
+    command -v lsb_release >/dev/null 2>&1 || { [ -e "/etc/euleros-release" ] && yum -y install euleros-lsb || yum -y install redhat-lsb-core; clear; }
+  fi
+  if [ -e "/usr/bin/apt-get" ]; then
+    PM=apt-get
+    command -v lsb_release >/dev/null 2>&1 || { apt-get -y update; apt-get -y install lsb-release; clear; }
+  fi
+
+  command -v lsb_release >/dev/null 2>&1 || { echo "${CFAILURE}${PM} source failed! ${CEND}"; kill -9 $$; }
+
+  # Get OS Version
+  if [ -e /etc/redhat-release ]; then
+    OS=CentOS
+    CentOS_ver=$(lsb_release -sr | awk -F. '{print $1}')
+    [ -n "$(lsb_release -is | grep -Ei 'Alibaba|Aliyun')" ] && { CentOS_ver=7; Aliyun_ver=$(lsb_release -rs); }
+    [[ "$(lsb_release -is)" =~ ^EulerOS$ ]] && { CentOS_ver=7; EulerOS_ver=$(lsb_release -rs); }
+    [ "$(lsb_release -is)" == 'Fedora' ] && [ ${CentOS_ver} -ge 19 >/dev/null 2>&1 ] && { CentOS_ver=7; Fedora_ver=$(lsb_release -rs); }
+  elif [ -n "$(grep 'Amazon Linux' /etc/issue)" -o -n "$(grep 'Amazon Linux' /etc/os-release)" ]; then
+    OS=CentOS
+    CentOS_ver=7
+  elif [ -n "$(grep 'bian' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Debian" ]; then
+    OS=Debian
+    Debian_ver=$(lsb_release -sr | awk -F. '{print $1}')
+  elif [ -n "$(grep 'Deepin' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Deepin" ]; then
+    OS=Debian
+    Debian_ver=8
+  elif [ -n "$(grep -w 'Kali' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Kali" ]; then
+    OS=Debian
+    if [ -n "$(grep 'VERSION="2016.*"' /etc/os-release)" ]; then
+      Debian_ver=8
+    elif [ -n "$(grep 'VERSION="2017.*"' /etc/os-release)" ]; then
+      Debian_ver=9
+    elif [ -n "$(grep 'VERSION="2018.*"' /etc/os-release)" ]; then
+      Debian_ver=9
+    fi
+  elif [ -n "$(grep 'Ubuntu' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Ubuntu" -o -n "$(grep 'Linux Mint' /etc/issue)" ]; then
+    OS=Ubuntu
+    Ubuntu_ver=$(lsb_release -sr | awk -F. '{print $1}')
+    [ -n "$(grep 'Linux Mint 18' /etc/issue)" ] && Ubuntu_ver=16
+  elif [ -n "$(grep 'elementary' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'elementary' ]; then
+    OS=Ubuntu
+    Ubuntu_ver=16
+  fi
+
+  # Check OS Version
+  if [ ${CentOS_ver} -lt 7 >/dev/null 2>&1 ] || [ ${Debian_ver} -lt 9 >/dev/null 2>&1 ] || [ ${Ubuntu_ver} -lt 18 >/dev/null 2>&1 ]; then
+    echo "${CFAILURE}此系统版本不支持, 请安装 CentOS 7+,Debian 8+,Ubuntu 16+ ${CEND}"
+    kill -9 $$
+  fi
+
+  command -v gcc > /dev/null 2>&1 || $PM -y install gcc
+  gcc_ver=$(gcc -dumpversion | awk -F. '{print $1}')
+
+  [ ${gcc_ver} -lt 5 >/dev/null 2>&1 ] && redis_ver=${redis_oldver}
+
+  if uname -m | grep -Eqi "arm|aarch64"; then
+    armplatform="y"
+    if uname -m | grep -Eqi "armv7"; then
+      TARGET_ARCH="armv7"
+    elif uname -m | grep -Eqi "armv8"; then
+      TARGET_ARCH="arm64"
+    elif uname -m | grep -Eqi "aarch64"; then
+      TARGET_ARCH="aarch64"
+    else
+      TARGET_ARCH="unknown"
+    fi
+  fi
+
+  if [ "$(uname -r | awk -F- '{print $3}' 2>/dev/null)" == "Microsoft" ]; then
+    Wsl=true
+  fi
+
+  if [ "$(getconf WORD_BIT)" == "32" ] && [ "$(getconf LONG_BIT)" == "64" ]; then
+    OS_BIT=64
+    SYS_BIT_j=x64 #jdk
+    SYS_BIT_a=x86_64 #mariadb
+    SYS_BIT_b=x86_64 #mariadb
+    SYS_BIT_c=x86_64 #ZendGuardLoader
+    SYS_BIT_d=x86-64 #ioncube
+    [ "${TARGET_ARCH}" == 'aarch64' ] && { SYS_BIT_c=aarch64; SYS_BIT_d=aarch64; }
+  else
+    OS_BIT=32
+    SYS_BIT_j=i586
+    SYS_BIT_a=x86
+    SYS_BIT_b=i686
+    SYS_BIT_c=i386
+    SYS_BIT_d=x86
+    [ "${TARGET_ARCH}" == 'armv7' ] && { SYS_BIT_c=armhf; SYS_BIT_d=armv7l; }
+  fi
+
+  THREAD=$(grep 'processor' /proc/cpuinfo | sort -u | wc -l)
+
+  # Percona binary: https://www.percona.com/doc/percona-server/5.7/installation.html#installing-percona-server-from-a-binary-tarball
+  if [ ${Debian_ver} -lt 9 >/dev/null 2>&1 ] || [ ${Ubuntu_ver} -lt 14 >/dev/null 2>&1 ]; then
+    sslLibVer=ssl100
+  elif [[ "${CentOS_ver}" =~ ^[6-7]$ ]] && [ "$(lsb_release -is)" != 'Fedora' ]; then
+    sslLibVer=ssl101
+  elif [ ${Debian_ver} -ge 9 >/dev/null 2>&1 ] || [ ${Ubuntu_ver} -ge 14 >/dev/null 2>&1 ]; then
+    sslLibVer=ssl102
+  elif [ ${Fedora_ver} -ge 27 >/dev/null 2>&1 ]; then
+    sslLibVer=ssl102
+  elif [ "${CentOS_ver}" == '8' ]; then
+    sslLibVer=ssl1:111
+  else
+    sslLibVer=unknown
+  fi
+}
+
+# 系统组件安装
+sys_plugins_install()
+{
+  check_os
+  # 安装 wget gcc curl git python
+  [ "${PM}" == 'apt-get' ] && apt-get -y update
+  [ "${PM}" == 'yum' ] && yum clean all
+  ${PM} -y install wget gcc curl python git
+  [ "${CentOS_ver}" == '8' ] && { yum -y install python36; sudo alternatives --set python /usr/bin/python3; }
+}
+
+# 安装docker docker-compose
+do_install_docker()
+{
+    curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+
+    sudo mkdir -p /etc/docker
+    sudo tee /etc/docker/daemon.json << EOF
+{
+  "registry-mirrors": ["https://f0tv1cst.mirror.aliyuncs.com"]
+}
+EOF
+  [ "${OS}" == "Debian" || "${OS}" == "Ubuntu" ] && sudo apt-get service docker start
+  [ "${OS}" == "CentOS" ] && sudo systemctl daemon-reload && sudo systemctl restart docker
+  # 安装 docker-compose
+  [ "${OS}" == "Debian" || "${OS}" == "Ubuntu" ] && sudo apt-get service docker start
+  [ "${OS}" == "CentOS" ] && sudo systemctl daemon-reload && sudo systemctl restart docker
+
+
+}
+
+# 安装GS组件
