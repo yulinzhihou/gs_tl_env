8c5947c3c7f2f36c1b9c35935860fea0bbde16f7
yulinzhihou <yulinzhihou@gmail.com>
2021-02-01 16:33:19 +0800

    2-1 更新docker国内安装源

--- install.sh
+++ install.sh
@@ -173,7 +173,7 @@ sys_plugins_install()
 # 安装docker docker-compose
 do_install_docker()
 {
-    curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+    curl -sSL https://get.daocloud.io/docker | sh
 
     sudo mkdir -p /etc/docker
     sudo tee /etc/docker/daemon.json << EOF
@@ -187,6 +187,9 @@ EOF
   [ "${OS}" == "Debian" || "${OS}" == "Ubuntu" ] && sudo apt-get service docker start
   [ "${OS}" == "CentOS" ] && sudo systemctl daemon-reload && sudo systemctl restart docker
 
+  curl -L https://get.daocloud.io/docker/compose/releases/download/1.28.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  docker-compose --version
 
 }
 
