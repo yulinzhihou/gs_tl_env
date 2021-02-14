786ccefd2c5e7fbd778919e12b47bf98e4be7100
yulinzhihou <yulinzhihou@gmail.com>
2021-02-14 08:42:10 +0800

    2-13 整合一个安装文件

--- install.sh
+++ install.sh
@@ -8,6 +8,16 @@
 # 第一步：更新系统组件，安装docker,docker-composer
 # 第二步：下载GS相关命令到系统
 # 第三步：在线下载打包好的镜像或者导入离线版本的镜像
+#!/usr/bin/env bash
+# Author: yulinzhihou <yulinzhihou@gmail.com>
+# Forum:  https://gsgamesahre.com
+# Project: https://github.com/yulinzhihou/gs_tl_env.git
+# Date :  2021-02-12
+# Notes:  GS_TL_Env for CentOS/RedHat 7+ Debian 10+ and Ubuntu 18+
+
+# 第一步：更新系统组件，安装docker,docker-composer
+# 第二步：下载GS相关命令到系统
+# 第三步：在线下载打包好的镜像或者导入离线版本的镜像
 startTime=`date +%s`
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
@@ -19,11 +29,137 @@ printf "
 #######################################################################
 "
 # 颜色代码
-. `dirname $0`/include/color.sh
+echo=echo
+for cmd in echo /bin/echo; do
+  $cmd >/dev/null 2>&1 || continue
+  if ! $cmd -e "" | grep -qE '^-e'; then
+    echo=$cmd
+    break
+  fi
+done
+CSI=$($echo -e "\033[")
+CEND="${CSI}0m"
+CDGREEN="${CSI}32m"
+CRED="${CSI}1;31m"
+CGREEN="${CSI}1;32m"
+CYELLOW="${CSI}1;33m"
+CBLUE="${CSI}1;34m"
+CMAGENTA="${CSI}1;35m"
+CCYAN="${CSI}1;36m"
+CSUCCESS="$CDGREEN"
+CFAILURE="$CRED"
+CQUESTION="$CMAGENTA"
+CWARNING="$CYELLOW"
+CMSG="$CCYAN"
 # 检测系统版本
-. `dirname $0`/include/check_os.sh
-# 加载shell
-. `dirname $0`/include/get_char.sh
+if [ -e "/usr/bin/yum" ]; then
+  PM=yum
+  command -v lsb_release >/dev/null 2>&1 || { [ -e "/etc/euleros-release" ] && yum -y install euleros-lsb || yum -y install redhat-lsb-core; clear; }
+fi
+if [ -e "/usr/bin/apt-get" ]; then
+  PM=apt-get
+  command -v lsb_release >/dev/null 2>&1 || { apt-get -y update; apt-get -y install lsb-release; clear; }
+fi
+
+command -v lsb_release >/dev/null 2>&1 || { echo "${CFAILURE}${PM} source failed! ${CEND}"; kill -9 $$; }
+
+# Get OS Version
+if [ -e /etc/redhat-release ]; then
+  OS=CentOS
+  CentOS_ver=$(lsb_release -sr | awk -F. '{print $1}')
+  [ -n "$(lsb_release -is | grep -Ei 'Alibaba|Aliyun')" ] && { CentOS_ver=7; Aliyun_ver=$(lsb_release -rs); }
+  [[ "$(lsb_release -is)" =~ ^EulerOS$ ]] && { CentOS_ver=7; EulerOS_ver=$(lsb_release -rs); }
+  [ "$(lsb_release -is)" == 'Fedora' ] && [ ${CentOS_ver} -ge 19 >/dev/null 2>&1 ] && { CentOS_ver=7; Fedora_ver=$(lsb_release -rs); }
+elif [ -n "$(grep 'Amazon Linux' /etc/issue)" -o -n "$(grep 'Amazon Linux' /etc/os-release)" ]; then
+  OS=CentOS
+  CentOS_ver=7
+elif [ -n "$(grep 'bian' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Debian" ]; then
+  OS=Debian
+  Debian_ver=$(lsb_release -sr | awk -F. '{print $1}')
+elif [ -n "$(grep 'Deepin' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Deepin" ]; then
+  OS=Debian
+  Debian_ver=8
+elif [ -n "$(grep -w 'Kali' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Kali" ]; then
+  OS=Debian
+  if [ -n "$(grep 'VERSION="2016.*"' /etc/os-release)" ]; then
+    Debian_ver=8
+  elif [ -n "$(grep 'VERSION="2017.*"' /etc/os-release)" ]; then
+    Debian_ver=9
+  elif [ -n "$(grep 'VERSION="2018.*"' /etc/os-release)" ]; then
+    Debian_ver=9
+  fi
+elif [ -n "$(grep 'Ubuntu' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Ubuntu" -o -n "$(grep 'Linux Mint' /etc/issue)" ]; then
+  OS=Ubuntu
+  Ubuntu_ver=$(lsb_release -sr | awk -F. '{print $1}')
+  [ -n "$(grep 'Linux Mint 18' /etc/issue)" ] && Ubuntu_ver=16
+elif [ -n "$(grep 'elementary' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'elementary' ]; then
+  OS=Ubuntu
+  Ubuntu_ver=16
+fi
+
+# Check OS Version
+if [ ${CentOS_ver} -lt 6 >/dev/null 2>&1 ] || [ ${Debian_ver} -lt 8 >/dev/null 2>&1 ] || [ ${Ubuntu_ver} -lt 14 >/dev/null 2>&1 ]; then
+  echo "${CFAILURE}Does not support this OS, Please install CentOS 6+,Debian 8+,Ubuntu 14+ ${CEND}"
+  kill -9 $$
+fi
+
+command -v gcc > /dev/null 2>&1 || $PM -y install gcc
+gcc_ver=$(gcc -dumpversion | awk -F. '{print $1}')
+
+[ ${gcc_ver} -lt 5 >/dev/null 2>&1 ] && redis_ver=${redis_oldver}
+
+if uname -m | grep -Eqi "arm|aarch64"; then
+  armplatform="y"
+  if uname -m | grep -Eqi "armv7"; then
+    TARGET_ARCH="armv7"
+  elif uname -m | grep -Eqi "armv8"; then
+    TARGET_ARCH="arm64"
+  elif uname -m | grep -Eqi "aarch64"; then
+    TARGET_ARCH="aarch64"
+  else
+    TARGET_ARCH="unknown"
+  fi
+fi
+
+if [ "$(uname -r | awk -F- '{print $3}' 2>/dev/null)" == "Microsoft" ]; then
+  Wsl=true
+fi
+
+if [ "$(getconf WORD_BIT)" == "32" ] && [ "$(getconf LONG_BIT)" == "64" ]; then
+  OS_BIT=64
+  SYS_BIT_j=x64 #jdk
+  SYS_BIT_a=x86_64 #mariadb
+  SYS_BIT_b=x86_64 #mariadb
+  SYS_BIT_c=x86_64 #ZendGuardLoader
+  SYS_BIT_d=x86-64 #ioncube
+  [ "${TARGET_ARCH}" == 'aarch64' ] && { SYS_BIT_c=aarch64; SYS_BIT_d=aarch64; }
+else
+  OS_BIT=32
+  SYS_BIT_j=i586
+  SYS_BIT_a=x86
+  SYS_BIT_b=i686
+  SYS_BIT_c=i386
+  SYS_BIT_d=x86
+  [ "${TARGET_ARCH}" == 'armv7' ] && { SYS_BIT_c=armhf; SYS_BIT_d=armv7l; }
+fi
+
+THREAD=$(grep 'processor' /proc/cpuinfo | sort -u | wc -l)
+
+# Percona binary: https://www.percona.com/doc/percona-server/5.7/installation.html#installing-percona-server-from-a-binary-tarball
+if [ ${Debian_ver} -lt 9 >/dev/null 2>&1 ] || [ ${Ubuntu_ver} -lt 14 >/dev/null 2>&1 ]; then
+  sslLibVer=ssl100
+elif [[ "${CentOS_ver}" =~ ^[6-7]$ ]] && [ "$(lsb_release -is)" != 'Fedora' ]; then
+  sslLibVer=ssl101
+elif [ ${Debian_ver} -ge 9 >/dev/null 2>&1 ] || [ ${Ubuntu_ver} -ge 14 >/dev/null 2>&1 ]; then
+  sslLibVer=ssl102
+elif [ ${Fedora_ver} -ge 27 >/dev/null 2>&1 ]; then
+  sslLibVer=ssl102
+elif [ "${CentOS_ver}" == '8' ]; then
+  sslLibVer=ssl1:111
+else
+  sslLibVer=unknown
+fi
+
 # 检测是不是root用户。不是则退出
 [ $(id -u) != "0" ] && { echo "${CFAILURE}错误: 你必须使用${CEND}"; exit 1; }
 # 系统组件安装
@@ -89,8 +225,6 @@ ini_config()
         echo 'RESTART="always"' >> /etc/profile
     fi
 
-
-#  get_char
   ARG_NUM=$#
   # 修改billing参数
   if [ -e "/etc/profile" ]; then
@@ -297,12 +431,12 @@ printf "
 #######################################################################
 #       GS_TL_Env 支持： CentOS/RedHat 7+  Ubuntu 18+ Debian 10+
 #       \e[44m GS游享网 [https://gsgameshare.com] 专用环境安装成功!\e[0m
-#       1.数据库端口: `echo [ ! -z ${TL_MYSQL_PORT} ] && ${TL_MYSQL_PORT} || 33061`
-#       2.数据库密码: `echo [ ! -z ${TL_MYSQL_PASSWORD} ] && ${TL_MYSQL_PASSWORD} || 123456`
-#       3.登录网关端口: `echo [ ! -z ${LOGIN_PORT} ] && ${LOGIN_PORT} || 13580`
-#       4.游戏网关端口: `echo [ ! -z ${SERVER_PORT} ] && ${SERVER_PORT} || 15680`
-#       5.网站端口: `echo [ ! -z ${WEB_PORT} ] && ${WEB_PORT} || 58080`
-#       6.Billing端口: `echo [ ! -z ${TL_MYSQL_PASSWORD} ] && ${TL_MYSQL_PASSWORD} || 21818`
+#       1.数据库端口: `[ ! -z ${TL_MYSQL_PORT} ] && ${TL_MYSQL_PORT} || 33061`
+#       2.数据库密码: `[ ! -z ${TL_MYSQL_PASSWORD} ] && ${TL_MYSQL_PASSWORD} || 123456`
+#       3.登录网关端口: `[ ! -z ${LOGIN_PORT} ] && ${LOGIN_PORT} || 13580`
+#       4.游戏网关端口: `[ ! -z ${SERVER_PORT} ] && ${SERVER_PORT} || 15680`
+#       5.网站端口: `[ ! -z ${WEB_PORT} ] && ${WEB_PORT} || 58080`
+#       6.Billing端口: `[ ! -z ${TL_MYSQL_PASSWORD} ] && ${TL_MYSQL_PASSWORD} || 21818`
 #       更多天龙网游单机版本请访问：          https://gsgamesahre.com
 #       技术交流群：                        826717146
 #######################################################################
