db8b61e7f4b4199efd2f205c9e0100d0b423b71f
yulinzhihou <yulinzhihou@gmail.com>
2021-03-05 16:04:41 +0800

    3-5 优化安装配置脚本-1

--- scripts/setconfig.sh
+++ scripts/setconfig.sh
@@ -4,21 +4,10 @@
 # Project: https://github.com/yulinzhihou/gs_tl_env.git
 # Date :  2021-02-01
 # Notes:  GS_TL_Env for CentOS/RedHat 7+ Debian 10+ and Ubuntu 18+
-# comment: 当用户需要重新生成数据库端口，密码时，则使用此命令进行重装写入配置，注意，执行完成后需要重启服务器再进行配置。否则需要使用 source /etc/profile.d 让数据临时生效
+# comment: 当用户需要重新生成数据库端口，密码时，则使用此命令进行重装写入配置，注意，执行完成后需要重启服务器再进行配置。否则需要使用 upenv.d 让数据临时生效
 # 修改billing参数
-##
-#BILLING_PORT=21818
-##
-#TL_MYSQL_PORT=33061
-##
-#LOGIN_PORT=13580
-##
-#SERVER_PORT=15680
-##
-#WEB_PORT=58080
-##
-#TL_MYSQL_PASSWORD=123456
-source /etc/profile
+upenv
+
 [ -z "`grep ^'export BILLING_PORT' /etc/profile`" ] && BILLING_PORT=${BILLING_DEFAULT_PORT} || BILLING_PORT=${BILLING_PORT}
 while :; do echo
   read -e -p "当前【Billing验证端口】为：${CBLUE}[${BILLING_PORT}]${CEND}，是否需要修改【Billing验证端口】 [y/n](默认: n): " IS_MODIFY
@@ -54,7 +43,7 @@ while :; do echo
 done
 
 # 修改mysql_Port参数
-source /etc/profile
+upenv
 [ -z "`grep ^'export TL_MYSQL_PORT' /etc/profile`" ] && TL_MYSQL_PORT=${TL_MYSQL_DEFAULT_PORT} || TL_MYSQL_PORT=${TL_MYSQL_PORT}
 while :; do echo
   read  -e -p "当前【mysql端口】为：${CBLUE}[${TL_MYSQL_PORT}]${CEND}，是否需要修改【mysql端口】 [y/n](默认: n): " IS_MODIFY
@@ -90,7 +79,7 @@ while :; do echo
 done
 
 # 修改login_Port参数
-source /etc/profile
+upenv
 [ -z "`grep ^'export LOGIN_PORT' /etc/profile`" ] && LOGIN_PORT=${LOGIN_DEFAULT_PORT} || LOGIN_PORT=${LOGIN_PORT}
 while :; do echo
   read  -e -p "当前【登录端口】为：${CBLUE}[${LOGIN_PORT}]${CEND}，是否需要修改【登录端口】 [y/n](默认: n): " IS_MODIFY
@@ -126,7 +115,7 @@ while :; do echo
 done
 
 # 修改Game_Port参数
-source /etc/profile
+upenv
 [ -z "`grep ^'export SERVER_PORT' /etc/profile`" ] && SERVER_PORT=${SERVER_DEFAULT_PORT} || SERVER_PORT=${SERVER_PORT}
 while :; do echo
   read  -e -p "当前【游戏端口】为：${CBLUE}[${SERVER_PORT}]${CEND}，是否需要修改【游戏端口】 [y/n](默认: n): " IS_MODIFY
@@ -162,7 +151,7 @@ while :; do echo
 done
 
 # 修改WEB_Port参数
-source /etc/profile
+upenv
 [ -z "`grep ^'export WEB_PORT' /etc/profile`" ] && WEB_PORT=${WEB_DEFAULT_PORT} || WEB_PORT=${WEB_PORT}
 while :; do echo
   read  -e -p "当前【网站端口】为：${CBLUE}[${WEB_PORT}]${CEND}，是否需要修改【网站端口】 [y/n](默认: n): " IS_MODIFY
@@ -198,7 +187,7 @@ while :; do echo
 done
 
 # 修改数据库密码
-source /etc/profile
+upenv
 [ -z "`grep ^'export TL_MYSQL_PASSWORD' /etc/profile`" ] && TL_MYSQL_PASSWORD=${TL_MYSQL_DEFAULT_PASSWORD} || TL_MYSQL_PASSWORD=${TL_MYSQL_PASSWORD}
 while :; do echo
   read  -e -p "当前【数据库密码】为：${CBLUE}[${TL_MYSQL_PASSWORD}]${CEND}，是否需要修改【数据库密码】 [y/n](默认: n): " IS_MODIFY
