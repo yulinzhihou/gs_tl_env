60da6c44e44d393d99ec6aa859fb30a21ad249a5
yulinzhihou <yulinzhihou@gmail.com>
2021-03-17 23:34:04 +0800

    3-17 增加网站开着命令

---
+++ scripts/upgm.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Author: yulinzhihou <yulinzhihou@gmail.com>
+# Forum:  https://gsgamesahre.com
+# Project: https://github.com/yulinzhihou/gs_tl_env.git
+# Date :  2021-02-01
+# Notes:  GS_TL_Env for CentOS/RedHat 7+ Debian 10+ and Ubuntu 18+
+# comment: 用来配置在线GM网站访问。默认网站内容请自觉上传到/tlgame/www/gsgm目录下
+if [ -f ./color.sh ]; then
+  . /root/.tlgame/scripts/color.sh
+else
+  . /usr/local/bin/color
+fi
